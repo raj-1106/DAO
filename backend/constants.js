51c5f123a748753b43e8bb8d49a3af542fc06616
Raj Lathigra <rajlathigra03@gmail.com>
2023-05-01 17:50:23 +0530

    .

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x2dDcbd9Bfe849EF972910bD453F170611FFD9e14";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
